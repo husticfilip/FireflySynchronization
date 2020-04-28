e4cc7a531e2922a8ab621cc776be8119d6f8efc8
jessicamegane <jessicacunha98@hotmail.com>
2020-04-28 18:29:40 +0100

    jess's code

---
+++ parameters.py
@@ -0,0 +1,18 @@
+
+
+experimentation = {
+    'NR_EXP': 1,
+    'SAVE_FOLDER': 'test/'
+}
+
+
+params = {
+    'POP_SIZE': 10,
+    'POSITION_MODE': 0,
+    'BLINKING_TIME': 2,
+    'SIMULATION_TIME': 10,
+    'FREQUENCY_MAX': 5,
+    'LATENCY_MAX': 5,
+    'X_MAX': 10,
+    'Y_MAX': 10,
+}
