b4c7a5e28b79750ee1a76f54cd6745c55e4ecac5
jessicamegane <jessicacunha98@hotmail.com>
2020-05-11 20:12:03 +0100

    knn and generate pop randomly

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import random
 import math
 from parameters import params, experimentation
+from sklearn.neighbors import KDTree
 from fireFly import FireFly
+import numpy as np
 
 
 #############################################
@@ -16,15 +18,18 @@ def generate_population_randomly():
     period_domain = [params['PERIOD_MIN'], params['PERIOD_MAX']]
     latency = params['LATENCY'] # we can change so latency will also between some min and max value
     subtraction_time = params['SUBTRACTION_TIME'] # time we substract from counter if we get signal from other firefly
-
+    period_threshold = 1
+    coords = []
     for id in range(params['POP_SIZE']):
         x_coord = random.randint(0, x_max)
         y_coord = random.randint(0,y_max)
+        coords.append([x_coord, y_coord])
+        latency = random.randint(0,10)
         period = random.uniform(period_domain[0], period_domain[1])
-        population.append(FireFly(id,x_coord, y_coord, period, subtraction_time, expFunct(), latency))
+        waiting_time = 1
+        population.append(FireFly(id,x_coord, y_coord, period, period_threshold,waiting_time, linearFunct(), linearFunct(), latency))
 
-    for firefily in population:
-        firefily.setNeighbours(find_n_nearest_neighbours(firefily, population))
+    find_n_nearest_neighbours(population, coords)
 
     return population
 
@@ -32,27 +37,15 @@ def generate_population_randomly():
 def generate_population_manualy():
     fireflyes = []
 
-    fireflyes.append(FireFly(1, 0, 0, 2.5, 1, contFunction(0.2), contFunction(0.8), 0))
-    fireflyes.append(FireFly(2, 1, 2, 4, 1, contFunction(0.2), contFunction(0.8), 1))
-    fireflyes.append(FireFly(3, 4, 5, 5.5, 1, contFunction(0.2), contFunction(0.8), 0))
-    fireflyes.append(FireFly(4, 9, 9, 7, 1, contFunction(0.2), contFunction(0.8), 3.5))
-    #fireflyes.append(FireFly(5, 9, 9, 2.3, 0.5, contFunction(0.01), contFunction(0.1), 6))
-    #fireflyes.append(FireFly(6, 9, 9, 14.5, 0.5, contFunction(0.01), contFunction(0.1), 2))
-    #fireflyes.append(FireFly(7, 9, 9, 6.5, 0.5, contFunction(0.01), contFunction(0.1), 0))
+    fireflyes.append(FireFly(1, 0, 0, 4, 1.5, 0.5, linearFunct(), linearFunct(), start_delay = 5))
+    fireflyes.append(FireFly(2, 1, 2, 3, 1.5, 0.5, linearFunct(), linearFunct(), start_delay = 1))
+    fireflyes.append(FireFly(3, 4, 5, 2.5, 1.5, 0.5, linearFunct(), linearFunct(), start_delay = 3))
+    fireflyes.append(FireFly(4, 9, 9, 7, 1.5, 0.5, linearFunct(), linearFunct(), start_delay = 7))
 
-    for i in range(len(fireflyes)):
-        neigh = []
-        for j in range(len(fireflyes)):
-            if j != i:
-                neigh.append(fireflyes[j])
-        fireflyes[i].setNeighbours(neigh)
-
-
-
-    # fireflyes[0].setNeighbours([fireflyes[1], fireflyes[2], fireflyes[3]])
-    # fireflyes[1].setNeighbours([fireflyes[0], fireflyes[2], fireflyes[3]])
-    # fireflyes[2].setNeighbours([fireflyes[3], fireflyes[0], fireflyes[1]])
-    # fireflyes[3].setNeighbours([fireflyes[0], fireflyes[1], fireflyes[2]])
+    fireflyes[0].setNeighbours([fireflyes[1], fireflyes[2], fireflyes[3]])
+    fireflyes[1].setNeighbours([fireflyes[0], fireflyes[2], fireflyes[3]])
+    fireflyes[2].setNeighbours([fireflyes[3], fireflyes[0], fireflyes[1]])
+    fireflyes[3].setNeighbours([fireflyes[0], fireflyes[1], fireflyes[2]])
 
     return fireflyes
 
@@ -64,11 +57,15 @@ def generate_population_manualy():
 #
 ################################################
 
-def expFunct(A=1, b=-0.2):
+def expFunct(A=0.1, b=1):
     def fun(x):
         return A * math.exp(b*x)
     return fun
 
+def linearFunct(m = 0.1, b = 0):
+    def fun(x):
+        return m * x + b
+    return fun
 
 def contFunction(A=0.05):
     def fun(x):
@@ -82,8 +79,16 @@ def contFunction(A=0.05):
 #
 ################################################
 
-def find_n_nearest_neighbours(firefly, fireflyes):
-    pass
 
+def find_n_nearest_neighbours(fireflyes, X):
+    neighbours = []
+    X = np.array(X)
+    tree = KDTree(X, leaf_size=2)
+    for i in range(len(fireflyes)):
+        _ , ind = tree.query([X[i]], k=params['N_NEIGHBOURS'] + 1)
+        for j in ind[0]:
+            if j != i:
+                neighbours.append(fireflyes[j])
+        fireflyes[i].setNeighbours(neighbours)
 
 
